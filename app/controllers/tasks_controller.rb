814ee6ab807ac47daba7ca9c27c41af340ff7fef
tech.child <wishborn2004+1@gmail.com>
2020-04-15 04:59:15 +0000

    first time

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,17 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   def index
-    @tasks = Task.all
+    # loginしてたら
+      # トップページに「自分が投稿したタスクの一覧を表示させる」
+    # してなかったら
+      # ログインページに飛ばせる
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    else
+      redirect_to login_url
+    end
   end
   
   def show
@@ -12,7 +22,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'taskが正常に投稿されました'
@@ -42,14 +52,21 @@ class TasksController < ApplicationController
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
-end
 
-private
-
-def set_task
-  @task = Task.find(params[:id])
-end
-
-def task_params
-  params.require(:task).permit(:content, :status)
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end  
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :status, presence:true, length: {maximum: 10}
   validates :content, presence:true, length: {maximum: 255}
+  
+  belongs_to :user
 end
